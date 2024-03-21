f2fa69b58ff65b4710a8adf22057ab671010d159
jaybtcher <142506030+Iantherose@users.noreply.github.com>
2024-03-21 11:09:31 +0000

    1

---
+++ totss/code2.js
@@ -0,0 +1,18 @@
+gdjs.logoCode = {};
+
+
+gdjs.logoCode.eventsList0 = function(runtimeScene) {
+
+};
+
+gdjs.logoCode.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+
+gdjs.logoCode.eventsList0(runtimeScene);
+
+return;
+
+}
+
+gdjs['logoCode'] = gdjs.logoCode;
